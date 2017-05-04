e8b597bee706fed3358da6037f1b230695d952f2
pedroteosousa <pedroteosousa@gmail.com>
2017-05-04 11:41:51 -0300

    added classes to everything

--- algs/bit.cpp
+++ algs/bit.cpp
@@ -1,15 +1,18 @@
 const int N = 1e6 + 5;
 
-int b[N];
+class BIT {
+    public:
+        int b[N];
 
-int update(int p, int val, int n) {
-    for(;p < n; p += p & -p) b[p] += val;
-}
+        int update(int p, int val, int n) {
+            for(;p < n; p += p & -p) b[p] += val;
+        }
 
-int getsum(int p) {
-    int sum = 0;
-    for(; p != 0; p -= p & -p) {
-	    sum += b[p];
-    }
-    return sum;
-}
+        int getsum(int p) {
+            int sum = 0;
+            for(; p != 0; p -= p & -p) {
+                sum += b[p];
+            }
+            return sum;
+        }
+};
--- algs/union-find.cpp
+++ algs/union-find.cpp
@@ -1,18 +1,28 @@
 const int N = 1e6 + 5;
 
-int p[N];
-int w[N];
+class UnionFind {
+    public:
+        int p[N];
+        int w[N];
 
-int find(int x) {
-    return p[x] = x==p[x]?x:find(p[x]);
-}
+        void UnionFind() {
+            for(int i=0;i<N;i++) {
+                p[i] = i;
+                w[i] = 1;
+            }
+        }
 
-void join(int a, int b) {
-    if ((a = find(a)) == (b = find(b))) return;
-    if (w[a] < w[b]) swap(a, b);
-    w[a] += w[b];
-    p[b] = a;
-}
+        int find(int x) {
+            return p[x] = x==p[x]?x:find(p[x]);
+        }
+
+        void join(int a, int b) {
+            if ((a = find(a)) == (b = find(b))) return;
+            if (w[a] < w[b]) swap(a, b);
+            w[a] += w[b];
+            p[b] = a;
+        }
+};
 
 int main() {
     int n;
