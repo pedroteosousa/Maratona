ded6f6a44f80c2a5f7dd6b6438928dbcc961743c
Pedro Teotonio de Sousa <pedrosousa@zillertal.linux.ime.usp.br>
2017-08-15 14:26:01 -0300

    updated files

--- algs/bit.cpp
+++ algs/bit.cpp
@@ -1,18 +1,15 @@
 const int N = 1e6 + 5;
 
-class BIT {
-    public:
-        int b[N];
+int b[N];
 
-        int update(int p, int val, int n) {
-            for(;p < n; p += p & -p) b[p] += val;
-        }
+int update(int p, int val, int n) {
+	for(;p < n; p += p & -p) b[p] += val;
+}
 
-        int getsum(int p) {
-            int sum = 0;
-            for(; p != 0; p -= p & -p) {
-                sum += b[p];
-            }
-            return sum;
-        }
-};
+int getsum(int p) {
+	int sum = 0;
+	for(; p != 0; p -= p & -p) {
+		sum += b[p];
+	}
+	return sum;
+}
--- algs/union-find.cpp
+++ algs/union-find.cpp
@@ -1,36 +1,26 @@
 const int N = 1e6 + 5;
 
-class UnionFind {
-    public:
-        int p[N];
-        int w[N];
+int p[N];
+int w[N];
 
-        void UnionFind() {
-            for(int i=0;i<N;i++) {
-                p[i] = i;
-                w[i] = 1;
-            }
-        }
+int find(int x) {
+	return p[x] = x==p[x]?x:find(p[x]);
+}
 
-        int find(int x) {
-            return p[x] = x==p[x]?x:find(p[x]);
-        }
-
-        void join(int a, int b) {
-            if ((a = find(a)) == (b = find(b))) return;
-            if (w[a] < w[b]) swap(a, b);
-            w[a] += w[b];
-            p[b] = a;
-        }
-};
+void join(int a, int b) {
+	if ((a = find(a)) == (b = find(b))) return;
+	if (w[a] < w[b]) swap(a, b);
+	w[a] += w[b];
+	p[b] = a;
+}
 
 int main() {
-    int n;
-    scanf("%d", &n);
-    for(int i=0;i<n;i++) {
-        p[i] = i;
-        w[i] = 1;
-    } 
-    return 0;
+	int n;
+	scanf("%d", &n);
+	for(int i=0;i<n;i++) {
+		p[i] = i;
+		w[i] = 1;
+	} 
+	return 0;
 }
 
